286b1d7fac618f045a5ed5a27367ec0f30a4aa46
sreeram3927 <sreeram292004@gmail.com>
2023-04-10 21:03:32 +0530

    create cameraFeed frame

--- gps_tab.py
+++ gps_tab.py
@@ -7,3 +7,11 @@ class GraphFrame(ttk.Frame):
 
         label = tk.Label(self, text='Graph')
         label.pack(side='top', padx=10, pady=10, fill='both', expand=True)
+        
+class CameraFeedFrame(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        label = tk.Label(self, text='Camera Feed')
+        label.pack(side='top', padx=10, pady=10, fill='both', expand=True)
+
