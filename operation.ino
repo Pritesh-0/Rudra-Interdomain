36cfb263ec810bb090d646e5e213baaf75cfff62
Pritesh-0 <priteshagrawal0@gmail.com>
2023-04-10 09:22:03 +0530

    added grip function

--- operation.ino
+++ operation.ino
@@ -1,12 +1,13 @@
 #include <Servo.h>
 
 Servo arm[2][2];
-const int arm_pin[2][2]={{p1,p2},{p3,p4}};
+const int arm_pin[2][2]={{8,9},{10,11}};
+const int gripper[2]={6,7};
 
 void setup() {
   for (int i=0;i<2;i++){
     for (int j=0;j<2;j++){
-      arm[i][j].attach(arm_pin[i][j])
+      arm[i][j].attach(arm_pin[i][j]);
     }
   }
 }
@@ -18,50 +19,56 @@ void loop() {
 
 
 
-void lift(Servo arm[2][2],int arm_no, int deg,char x){
-  switch(x){
-    case 'u':
-      deg+=0;
-      break;
-    case 'd':
-      deg+=90;
-      break;
+void lift(Servo arm[2][2],int arm_no, int deg,int pos){
+  if(pos==1){
+    deg+=0;
+  }
+  else{
+    deg+=90;
   }
   arm[0][arm_no].write(deg);
 } 
 
 
-void turn(Servo arm[2][2],int deg,char x){
-  switch(x){
-    case 'r':
-      deg+=0;
-      break;
-    case 'l':
-      deg+=90;
-      break;
+void turn(Servo arm[2][2],int arm_no,int deg,int pos){
+  if(pos==1){
+    deg+=0;
   }
-  arm[0][arm_no].write(deg);
+  else{
+    deg+=90;
+  }
+  arm[1][arm_no].write(deg);
 }
+
+void grip(gripper[2],int arm_no,int x){
+  if (x){
+    digitalWrite(gripper[arm_no],HIGH);
+  }
+  else{
+    digitalWrite(gripper[arm_no],LOW);
+  }
+} 
 
 
 void move(int d1, int d2){
   //touch(arm1_claw,1)
-  lift(arm,0,d1,'u');
+  lift(arm,0,d1,1);
   delay(1000);
-  turn(arm,0,d2,'r');
+  turn(arm,0,d2,1);
   delay(1000);
-  lift(arm,0,d1,'d');
+  lift(arm,0,d1,0);
   delay(1000);
   //touch(arm1_claw,0);
 
   //touch(arm2_claw,1);
-  lift(arm,1,d1,'u');
+  lift(arm,1,d1,1);
   delay(1000);
-  turn(arm,0,0,'l');
+  turn(arm,0,0,0);
   delay(1000);
-  turn(arm,1,d2,'r');
+  turn(arm,1,d2,1);
   delay(1000);
-  lift(arm,1,d1,'d');
+  lift(arm,1,d1,0);
   delay(1000);
   //touch(arm2_claw,0);
 }
+
