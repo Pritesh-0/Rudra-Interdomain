30869e44a29a21056e71f7d27fff755c90d8b598
sreeram3927 <sreeram292004@gmail.com>
2023-04-10 20:43:50 +0530

    create standard frame class

--- tabs.py
+++ tabs.py
@@ -9,4 +9,14 @@ class GPSFrame(ttk.Frame):
 
         self.temp_value = tk.Label(self, text='3')
         self.temp_value.pack(side='left', padx=10, pady=10)
+        
+class StandardFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.angle_label = tk.Label(self, text='Stats;AnalyzedData;')
+        self.angle_label.pack(side='left', padx=10, pady=10)
+
+        self.angle_value = tk.Label(self, text='2')
+        self.angle_value.pack(side='left', padx=10, pady=10)
+
 
