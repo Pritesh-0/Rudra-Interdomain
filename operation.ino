6e356196e4ed9f00a451e7ff928067e88495d301
Pritesh-0 <priteshagrawal0@gmail.com>
2023-04-12 18:32:24 +0530

    added mission and grip function with l298n

--- operation.ino
+++ operation.ino
@@ -1,15 +1,24 @@
 #include <Servo.h>
 
 Servo arm[2][2];
-const int arm_pin[2][2]={{8,9},{10,11}};
-const int gripper[2]={6,7};
+const int arm_pin[2][2]={{l,t},{a1,a2}};
+const int gripper[2][2]={{g11,g12},{g21,g22}};
+const int misson[2]={{m11,m12},{m21,m22}};
 
 void setup() {
   for (int i=0;i<2;i++){
     for (int j=0;j<2;j++){
       arm[i][j].attach(arm_pin[i][j]);
+      pinMode(gripper[i][j],OUTPUT);
     }
   }
+
+  for (int i=0;i<2;i++){
+    for (int j=0;j<2;j++){
+      digitalWrite(gripper[i][j], LOW);
+    }
+  }
+  
 }
 
 void loop() {
@@ -17,6 +26,22 @@ void loop() {
   delay(3000);   
 }
 
+
+
+void drill(misson[2],int arm_no,int x){
+  if (x==1){
+    digitalWrite(misson[arm_no][0],HIGH);
+    digitalWrite(misson[arm_no][1],LOW);
+  }
+  else if(x==2){
+    digitalWrite(misson[arm_no][1],HIGH);
+    digitalWrite(misson[arm_no][0],LOW);
+  }
+  else{
+    digitalWrite(misson[arm_no][0],LOW);
+    digitalWrite(misson[arm_no][1],LOW);
+  }
+} 
 
 
 void lift(Servo arm[2][2],int arm_no, int deg,int pos){
@@ -41,26 +66,32 @@ void turn(Servo arm[2][2],int arm_no,int deg,int pos){
 }
 
 void grip(gripper[2],int arm_no,int x){
-  if (x){
-    digitalWrite(gripper[arm_no],HIGH);
+  if (x==1){
+    digitalWrite(gripper[arm_no][0],HIGH);
+    digitalWrite(gripper[arm_no][1],LOW);
+  }
+  else if(x==2){
+    digitalWrite(gripper[arm_no][1],HIGH);
+    digitalWrite(gripper[arm_no][0],LOW);
   }
   else{
-    digitalWrite(gripper[arm_no],LOW);
+    digitalWrite(gripper[arm_no][0],LOW);
+    digitalWrite(gripper[arm_no][1],LOW);
   }
 } 
 
 
 void move(int d1, int d2){
-  //touch(arm1_claw,1)
+  grip(gripper,0,1);
   lift(arm,0,d1,1);
   delay(1000);
   turn(arm,0,d2,1);
   delay(1000);
   lift(arm,0,d1,0);
   delay(1000);
-  //touch(arm1_claw,0);
+  grip(gripper,0,0);
 
-  //touch(arm2_claw,1);
+  grip(gripper,1,1);
   lift(arm,1,d1,1);
   delay(1000);
   turn(arm,0,0,0);
@@ -69,6 +100,6 @@ void move(int d1, int d2){
   delay(1000);
   lift(arm,1,d1,0);
   delay(1000);
-  //touch(arm2_claw,0);
+  grip(gripper,1,0);
 }
 
